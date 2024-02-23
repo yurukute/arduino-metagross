cbbafed3e1264ff3cfc697f61576cbb2efa9eb30
yurukute <yurukute@gmail.com>
2024-02-23 08:47:43 +0700

    Fix shaking servos
    - Reduce max pwm to adapt with the robot frame.
    - Implement init state for servos in coxa position.
    
    Branch: leg-movement

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #define B_LEFT  3
 #define B_RIGHT 4
 #define PWM_MIN  150
-#define PWM_MAX  600
+#define PWM_MAX  450
 
 struct leg {
     int coxa;
@@ -17,29 +17,51 @@ struct leg {
 struct leg legs[4];
 PCA9685 pwm(0x40);
 
-void servoWrite(uint8_t channel, int deg) {
+int servoWrite(uint8_t channel, int deg) {
     uint16_t pulselen = map(deg, 0, 180, PWM_MIN, PWM_MAX);
-    pwm.setPWM(channel, pulselen);
-}
+    if (pwm.isConnected())
+        pwm.setPWM(channel, pulselen);
+    else
+        Serial.println("PWM not available");
+    if (pwm.lastError() == PCA9685_OK) {
+        Serial.print("channel " + String(channel) + ": " + deg);
+        Serial.println(" (" + String(pulselen) + "WPM)");
+    }
+    delay(500);
+    return pwm.lastError();
 
+}
 void setup() {
     Serial.begin(9600);
     Wire.begin();
     pwm.begin();
     pwm.setFrequency(50);
-    
+
     for (int i = 0; i < 4; i++) {
         legs[i].tibia = 3*i;
         legs[i].femur = 3*i + 1;
         legs[i].coxa  = 3*i + 2;
-
     }
-    
-    servoWrite(legs[0].tibia, 90);
-    servoWrite(legs[0].femur, 90);
-    servoWrite(legs[0].coxa,  90);
+}
+
+void stop(int deg=90) {
+    for (int i = 0; i < 4; i++) {
+        servoWrite(legs[i].coxa, deg);
+    }
+}
+
+void sweep(int from, int to) {
+    int direction = 1;
+    if (from > to) {
+        direction = -1;
+    }
+    for (int deg = from; deg != to; deg+=10*(direction)) {
+        for (int i = 0; i < 4; i++) {
+            servoWrite(legs[i].coxa, deg);
+        }
+    }
 }
 
 void loop() {
-
+    stop();
 }
